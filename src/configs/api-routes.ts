1f3c0db4e1647bb4f606f339af0a1f87938c1805
antonio.junior <antoniodbjunior21@gmail.com>
2022-12-29 15:17:00 -0400

    register url change

--- src/configs/api-routes.ts
+++ src/configs/api-routes.ts
@@ -1,9 +1,9 @@
-export const apiHost = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api/v1' : 'https://myapp.herokuapp.com/api';
+export const apiHost = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api/v1' : 'http://localhost:8080/api/v1';
 
 export default  {
   meEndpoint: apiHost + '/auth/me',
   loginEndpoint: apiHost + '/auth/login',
-  registerEndpoint: apiHost + '/jwt/register',
+  registerEndpoint: apiHost + '/auth/register',
   storageTokenKeyName: 'accessToken',
   onTokenExpiration: 'refreshToken' // logout | refreshToken
 }
